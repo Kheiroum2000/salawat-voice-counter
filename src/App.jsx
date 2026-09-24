351d8d023e34d32a7c24e4149fd149412032e45a
Kheiroum2000 <kheiroum.jehangir@gmail.com>
2026-09-24 20:53:41 +0500

    Added levenshtein distance

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,44 @@ const target = "Sallallahu ala Muhammad"
 recognition.continuous = true;
 recognition.start();
 
-// function levenshteinDistance (string1 , string2){
-//   return(
-//     console.log(0)
-//   )
-// }
+function levenshteinDistance (string1 , string2){
+
+  const string1Len = string1.length
+  const string2Len = string2.length
+  const matrix = []
+
+  for(i = 0; i > string1Len; i ++){
+    matrix.push([])
+
+    for(j=0; j < string2Len; j++){
+      if(i===0){
+        matrix[i].push(j);
+    
+      } else if(j===0){
+        matrix[i].push(i)
+      } else {
+        const char1 = string1[i - 1]
+        const char2 = string2[j - 1]
+
+        if(char1 === char2){
+          matrix[i][j] = matrix[i - 1][j - 1];
+
+        }else {
+          matrix[i][j] =
+            Math.min(
+              matrix[i][j - 1],
+              matrix[i - 1][j],
+              matrix[i - 1][j - 1]
+            ) + 1;
+        }
+      }
+    }
+  }
+
+  return(
+    matrix[string1Len][string2Len]
+  )
+}
 
 
 
@@ -19,7 +52,6 @@ function App (){
 
   recognition.onresult = (event) =>{
   const transcript = event.results[0][0].transcript
-  console.log("RESULT EVENT FIRED");
   console.log(event.resultIndex)
   console.log(transcript)
   
